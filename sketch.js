1a7c1205de5e497c636f978f0f6c1539fd8a421d
codingwithvb <72670340+codingwithvb@users.noreply.github.com>
2021-03-22 18:54:24 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 var PROBLEM = 1; 
 var CORRECT = 2; 
 var WRONG = 3; 
+var SESSION_REPORT = 4; 
 var gameState = PROBLEM; 
 var rand1; 
 var rand2;  
+var correctAnswer; 
+var incorrectAnswer; 
 var playerInput; 
 var timer; 
+var totalQuestion; 
 var sessionTimer; 
 
 function setup() {
@@ -14,6 +18,8 @@ function setup() {
   this.input = createInput("Answer");
   this.button = createButton("Check Answer!"); 
   this.next = createButton("Next Question"); 
+  this.session = createButton("End Session"); 
+
   this.next.position(displayWidth/2 + 150, 100);
 
   if(gameState === PROBLEM){
@@ -25,10 +31,17 @@ function setup() {
 
   timer = 0; 
   sessionTimer = 0; 
+  incorrectAnswer = 0;
+  correctAnswer = 0;  
+  totalQuestion = 0; 
 }
 
 function draw() {
   background(220);
+
+  if(correctAnswer === undefined){
+    correctAnswer = 0; 
+  }
 
   if(gameState === PROBLEM){
 
@@ -39,7 +52,8 @@ function draw() {
   
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 30);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-    this.next.position(displayWidth/2 + 150, 100); 
+    this.next.position(displayWidth/2 + 150, 100);
+    this.session.position(displayWidth/2 + 150, 75);  
 
     var correctAnswer = rand1 * rand2; 
 
@@ -61,12 +75,16 @@ function draw() {
       var playerAnswer = parseInt(playerInput)
 
       if(playerAnswer === correctAnswer){
+        console.log(gameState);
         gameState = CORRECT;  
-        console.log(gameState); 
+        totalQuestion++; 
       }
 
       if(playerAnswer !== correctAnswer){
+
         gameState = WRONG; 
+        totalQuestion++; 
+        incorrectAnswer++; 
       }
     });
 
@@ -77,6 +95,11 @@ function draw() {
     timer = 0; 
 
     }); 
+
+    this.session.mousePressed(()=>{
+      gameState = SESSION_REPORT; 
+    }); 
+
   }
 
   if(gameState === CORRECT){
@@ -100,7 +123,7 @@ function draw() {
   }
 
   if(gameState === WRONG){
-
+ 
     textSize(50); 
     text(rand1, displayWidth/2,displayHeight/2-100);
     text("x " + rand2, displayWidth/2 - 35, displayHeight/2 -60);
@@ -118,5 +141,24 @@ function draw() {
   
       }); 
 
+  }
+
+  if(gameState === SESSION_REPORT){
+    this.input.hide();
+    this.button.hide();
+    this.next.hide();
+    this.session.hide(); 
+
+    correctAnswer = totalQuestion - incorrectAnswer; 
+
+    textSize(50); 
+    text("Session Report", displayWidth/2 - 100, 100);
+    textSize(25); 
+    text("Time Studied: " + sessionTimer + " second/s", displayWidth/2 - 100, 200);
+
+    text("Number of Questions: " + totalQuestion, displayWidth/2 - 100, 300); 
+
+   text("Correct: " + correctAnswer, displayWidth/2 - 100, 400); 
+   text("Incorrect: " + incorrectAnswer, displayWidth/2 - 100, 500); 
   }
 }
